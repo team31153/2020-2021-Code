4c9f5e7a3b77403571684734af90f51e95e2fdd2
Ashu Nanekar <ashutosh.nanekar@emc.com>
2020-10-24 13:47:56 -0400

    Worked a little bit on run 1.

--- robot/Run1/main.py
+++ robot/Run1/main.py
@@ -27,46 +27,110 @@ robot = DriveBase(cMotor, dMotor, 56, 60)
 
 # Write your program here.
 ev3.speaker.beep()
-def config():
-    times = 0
-    times2 = 0
-    while times < 41:
-        
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+import time
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+#initialize color sensors
+p1BSensor = ColorSensor(Port.S1)
+p2SSensor = ColorSensor(Port.S2)
+p3FSensor = ColorSensor(Port.S3)
+def colorConfig():
+
+    frontWhite = -1000
+    sideWhite = -1000
+    backWhite = -1000
+
+    while frontWhite == -1000 or sideWhite == -1000 or backWhite == -1000:
         listOfButtons = ev3.buttons.pressed()
         for oneButton in listOfButtons:
-            if oneButton == Button.UP:
-                whiteValueFront = frontColorSensor.reflection()
-                print("Front White Value" + str(whiteValueFront))
-                    
-            if oneButton == Button.DOWN:
-                whiteValueBack = backColorSensor.reflection()
-                print("Back White Value" + str(whiteValueBack))
-            
-        times = times + 0.001
+            print(oneButton)
+            if oneButton == Button.RIGHT:
+                sideWhite = p2SSensor.reflection()
+                print("Side value is: " + str(sideWhite))
+                ev3.screen.clear()
+                ev3.screen.draw_text(0, 50, "Side color sensor")
+                ev3.screen.draw_text(0, 70, "white: " + str(sideWhite))
+            elif oneButton == Button.UP:
+                frontWhite = p3FSensor.reflection()
+                print("Front value is: " + str(frontWhite))
+                ev3.screen.clear()
+                ev3.screen.draw_text(0, 50, "Front color sensor")
+                ev3.screen.draw_text(0, 70, "white: " + str(frontWhite))
+            elif oneButton == Button.DOWN:
+                backWhite = p1BSensor.reflection()
+                print("Back value is: " + str(backWhite))
+                ev3.screen.clear()
+                ev3.screen.draw_text(0, 50, "Back color sensor")
+                ev3.screen.draw_text(0, 70, "white: " + str(backWhite))
 
+    print("White sensing is done.") 
+    ev3.screen.clear()
+    ev3.screen.draw_text(0,50, "White sensing")
+    ev3.screen.draw_text(0,70, "is done.")
     time.sleep(2)
-    ev3.speaker.beep()
 
-    while times2 < 41:
+    print("Starting sensing for black.")
+    ev3.screen.clear()
+    ev3.screen.draw_text(0,50, "Starting sensing")
+    ev3.screen.draw_text(0,70, "for black.")
+    frontBlack = 1000
+    sideBlack = 1000
+    backBlack = 1000
+
+    while frontBlack == 1000 or sideBlack == 1000 or backBlack == 1000:
         listOfButtons = ev3.buttons.pressed()
         for oneButton in listOfButtons:
-            if oneButton == Button.UP:
-                blackValueFront = frontColorSensor.reflection()
-                print("Front Black Value" + str(blackValueFront))
-
-            if oneButton == Button.DOWN:
-                blackValueBack = backColorSensor.reflection()
-                print("Back Black Value" + str(blackValueBack))
-    
-        times2 = times2 + 0.001
-    ev3.speaker.beep()
-
-    f = open("blackValueBack.txt","w")
-    f.write(str(blackValueBack)+'\n')
-    f.write(str(blackValueFront)+'\n')
-    f.write(str(whiteValueBack)+'\n')
-    f.write(str(whiteValueFront)+'\n')
+            print(oneButton)
+            if oneButton == Button.RIGHT:
+                sideBlack = p2SSensor.reflection()
+                print("Side value is: " + str(sideBlack))
+                ev3.screen.clear()
+                ev3.screen.draw_text(0, 50, "Side color sensor")
+                ev3.screen.draw_text(0, 70, "black: " + str(sideBlack))
+            elif oneButton == Button.UP:
+                frontBlack = p3FSensor.reflection()
+                print("Front value is: " + str(frontBlack))
+                ev3.screen.clear()
+                ev3.screen.draw_text(0, 50, "Front color sensor")
+                ev3.screen.draw_text(0, 70, "black: " + str(frontBlack))
+            elif oneButton == Button.DOWN:
+                backBlack = p1BSensor.reflection()
+                print("Back value is: " + str(backBlack))
+                ev3.screen.clear()
+                ev3.screen.draw_text(0, 50, "Back color sensor")
+                ev3.screen.draw_text(0, 70, "black: " + str(backBlack))
+    print("Black sensing is done.")
+    ev3.screen.clear()
+    ev3.screen.draw_text(0,50, "Black sensing")
+    ev3.screen.draw_text(0,70, "is done.")
+    ev3.screen.draw_text(0,90, "Writing file.")
+    print("writing file")
+    #Write values to file
+    f = open("ConfiguredColor.txt", "w")
+    f.write(str(frontWhite)+'\n')
+    f.write(str(backWhite)+'\n')
+    f.write(str(sideWhite)+'\n')
+    f.write(str(frontBlack)+'\n')
+    f.write(str(backBlack)+'\n')
+    f.write(str(sideBlack)+'\n')
     f.close()
+    print("finished writing file")
+    ev3.screen.clear()
+    ev3.screen.draw_text(0,50, "Finished")
+    ev3.screen.draw_text(0,70, "writing file")
 def lineFollower(whiteValue, blackValue, lineFollowingDistance):
     distanceTraveled = robot.distance()
 
