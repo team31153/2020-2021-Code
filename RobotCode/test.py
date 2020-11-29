d9ac9ebdf8ce76ce948a76f48d69becd09ece9fd
Ryan-919 <68666490+Ryan-919@users.noreply.github.com>
2020-11-29 13:48:18 -0500

    Improved gradual gyro turn and tested medium motor

---
+++ RobotCode/test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+import time
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+#Initialize Motors
+cMotor = Motor(Port.C)
+dMotor = Motor(Port.D)
+robot = DriveBase(cMotor, dMotor, 55, 104)
+aMotor = Motor(Port.A)
+bMotor = Motor(Port.B)
+
+#initialize color sensors
+p1BSensor = ColorSensor(Port.S1)
+p2SSensor = ColorSensor(Port.S2)
+p3FSensor = ColorSensor(Port.S3)
+p4GSensor = GyroSensor(Port.S4)
+
+#bMotor.run_angle(700, 10000)
+aMotor.run_angle(1000, -5000)
