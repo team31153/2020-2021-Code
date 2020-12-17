1a7e4e3412fe492419a0eee42d4468d582ee2b95
Leonidas-brrt <69121425+Aaditya-brrt@users.noreply.github.com>
2020-12-16 19:15:08 -0500

    Add files via upload

--- RobotCode/Run2.py
+++ RobotCode/Run2.py
@@ -5,20 +5,8 @@ from GradualGyroB import *
 from GradualGyroF import *
 from TurnGradualGyro import *
 
-readAllValues()
-
-def Run2():
-    # Write Code Here:
-    gradualGyroBackward(200, 100)
-    turnGradualGyro(26)
-    gradualGyroBackward(150,50)
-    turnGradualGyro(-10)
-    gradualGyroForward(100, 50)
-    turnGradualGyro(-20)
-    gradualGyroBackward(150, 30)
-    turnGradualGyro(6)
-    aMotor.run_angle(10000, -5000)
-    gradualGyroForward(70, 90)
-    turnGradualGyro(-20)
-
-Run2()
+gradualGyroBackward(200, 100)
+turnGradualGyro(10)
+gradualGyroBackward(190,70)
+turnGradualGyro(-15)
+turnGradualGyro(15)
