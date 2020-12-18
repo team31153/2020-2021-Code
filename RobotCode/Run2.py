fcdf37c592d76f9d1bd90bef19802ca6c7550177
Leonidas-brrt <69121425+Aaditya-brrt@users.noreply.github.com>
2020-12-17 20:19:22 -0500

    Add files via upload

--- RobotCode/Run2.py
+++ RobotCode/Run2.py
@@ -4,9 +4,13 @@ from ColorSensorFunctions import *
 from GradualGyroB import *
 from GradualGyroF import *
 from TurnGradualGyro import *
+def Run2():
+    gradualGyroBackward(200, 100)
+    turnGradualGyro(10)
+    gradualGyroBackward(190,70)
+    turnGradualGyro(-15)
+    turnGradualGyro(15)
+    gradualGyroBackward(120, 30)
 
-gradualGyroBackward(200, 100)
-turnGradualGyro(10)
-gradualGyroBackward(190,70)
-turnGradualGyro(-15)
-turnGradualGyro(15)
+
+Run2()
