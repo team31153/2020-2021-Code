72aca909fc3b1c70abbd46d6c3d9d91bdeec5cb3
Ryan-919 <68666490+Ryan-919@users.noreply.github.com>
2020-12-26 18:56:49 -0500

    Worked on run 3 more

--- RobotCode/MotorControlRun3.py
+++ RobotCode/MotorControlRun3.py
@@ -8,9 +8,9 @@ while True:
         if oneButton == Button.UP:
             aMotor.run_angle(10000, 4500)
         if oneButton == Button.RIGHT:
-            bMotor.run_angle(1000, 5700)
+            bMotor.run_angle(1000, 2700)
         if oneButton == Button.DOWN:
             aMotor.run_angle(10000, -4500)
         if oneButton == Button.LEFT:
-            bMotor.run_angle(1000, -5700)
+            bMotor.run_angle(1000, -2700)
         
