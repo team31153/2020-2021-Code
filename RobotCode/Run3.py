5cf83c3750a737eb06f2a5c6c33a44eeed8addd8
Ryan-919 <68666490+Ryan-919@users.noreply.github.com>
2021-01-01 18:40:38 -0500

    Worked on run 3

--- RobotCode/Run3.py
+++ RobotCode/Run3.py
@@ -14,10 +14,11 @@ p4GSensor.reset_angle(0)
 def Run3():
 
     # Get out of base to go on line
-    gradualGyroBackward(650, 150)
+    gradualGyroBackward(650, 100)
     # Line follows
     # print("before line follower")
-    backLineFollowerRun3(-60)
+    backLineFollowerRun3(-50)
+    gradualGyroBackward(10, 30)
     # print("after line follower")
     # Turns
     #robot.turn(106)
@@ -29,6 +30,8 @@ def Run3():
     t1 = Thread(target=gradualGyroForward, args=([65, 40]))
     t1.start()
     aMotor.run_angle(1560, -3000)
+    # stopOnBlackS()
+
 
     run3Pt2()
     bMotor.run_angle(1560, 2800)
@@ -36,11 +39,20 @@ def Run3():
     readAllValues()
     stopOnBlackF()
     p4GSensor.reset_angle(0)
-    turnGradualGyro(60)
-    gradualGyroBackward(50, 50)
     turnGradualGyro(40)
-    gradualGyroBackward(30, 20)
+    gradualGyroForward(20, 30)
+    turnGradualGyro(30)
+    gradualGyroBackward(40, 30)
+    turnGradualGyro(-35)
+    # gradualGyroBackward(25)
+    # turnGradualGyro(-30)
     # run3Pt3()
+
+    def dance():
+        for i in range(12):
+            robot.forward(10)
+            robot.backward(10)
+            robot.right(30)
 
 
 
